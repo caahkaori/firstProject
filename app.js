ce9f76318902c8540337e2adb17e26231c1f6af4
caahkaori <carolinetakano8@gmail.com>
2020-06-23 21:24:09 +0900

    Second commit

--- app.js
+++ app.js
@@ -3,6 +3,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require('lodash');
+
+
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -14,34 +17,44 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 
-app.get("/", function(req, res){
-  res.render("home", {startingEJS: homeStartingContent});
+
+app.get("/", function(req, res) {
+  res.render("home", {
+    startingEJS: homeStartingContent,
+    posts: posts
+  });
 
 
 });
 
-app.get("/about", function(req, res){
-  res.render("about", {startingAbout: aboutContent});
+app.get("/about", function(req, res) {
+  res.render("about", {
+    startingAbout: aboutContent
+  });
 
 
 });
 
-app.get("/contact", function(req, res){
-  res.render("contact", {startingContact: contactContent});
+app.get("/contact", function(req, res) {
+  res.render("contact", {
+    startingContact: contactContent
+  });
 
 
 });
 
-app.get("/compose", function(req, res){
+app.get("/compose", function(req, res) {
   res.render("compose");
 
 
 });
 
-app.post("/compose", function(req, res){
+app.post("/compose", function(req, res) {
 
   const post = {
     title: req.body.titleBox,
@@ -50,8 +63,6 @@ app.post("/compose", function(req, res){
 
   posts.push(post);
 
-
-console.log(posts);
 
   // let post = [];
   //
@@ -65,6 +76,24 @@ console.log(posts);
 });
 
 
+app.get('/posts/:postName', function(req, res) {
+
+  const requestTitle = _.lowerCase(req.params.postName)
+
+  posts.forEach(function(post) {
+    const storedTitle = _.lowerCase(post.title)
+
+    if (storedTitle === (requestTitle)) {
+      res.render("post", {
+        title: post.title,
+        body: post.body
+      });
+    }
+
+  })
+
+
+});
 
 
 
